f2e2007a2d9f4edb117b1f5c04f29b45c7e13423
Mauro Perez <maurop123@gmail.com>
2015-09-17 07:18:58 -0400

    todo lists and trackers

---
+++ server/bootstrap.js
@@ -0,0 +1,47 @@
+// if the database is empty on server start, create some sample data.
+Meteor.startup(function () {
+  if (Lists.find().count() === 0) {
+    var data = [
+      {name: "Trackers",
+       items: ["Sex",
+         "Arrive @ Work",
+         "Eat Lunch"
+       ]
+      },
+      {name: "Languages",
+       items: ["Lisp",
+         "C",
+         "C++",
+         "Python",
+         "Ruby",
+         "JavaScript",
+         "Scala",
+         "Erlang",
+         "6502 Assembly"
+         ]
+      },
+      {name: "Favorite Scientists",
+       items: ["Ada Lovelace",
+         "Grace Hopper",
+         "Marie Curie",
+         "Carl Friedrich Gauss",
+         "Nikola Tesla",
+         "Claude Shannon"
+       ]
+      }
+    ];
+
+    var timestamp = (new Date()).getTime();
+    _.each(data, function(list) {
+      var list_id = Lists.insert({name: list.name,
+        incompleteCount: list.items.length});
+
+      _.each(list.items, function(text) {
+        Todos.insert({listId: list_id,
+                      text: text,
+                      createdAt: new Date(timestamp)});
+        timestamp += 1; // ensure unique timestamp.
+      });
+    });
+  }
+});
